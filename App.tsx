f96a5eec6915e6611d17135426e56d2cb98fec66
Adeen Ayub Vine <adeen.ayub.vine@gmail.com>
2024-06-01 15:08:50 +0500

    Dark and light mode added

--- App.tsx
+++ App.tsx
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Switch,
+  SafeAreaView,
+} from "react-native";
+import { ThemeContext } from "./src/context/ThemeContext";
+import { myColors } from "./src/styles/Colors";
+import Screen from "./src/components/Screen";
+import Icon from "react-native-vector-icons/Ionicons";
 
 export default function App() {
+  const [theme, setTheme] = useState("light");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView
+        style={
+          theme === "light"
+            ? styles.container
+            : [styles.container, { backgroundColor: "#000" }]
+        }
+      >
+        <StatusBar style="auto" />
+        {/* <Switch
+          value={theme === "light"} //if light then value is true
+          onValueChange={() => setTheme(theme === "light" ? "dark" : "light")}
+        /> */}
+        <TouchableOpacity
+          onPress={() => setTheme(theme === "light" ? "dark" : "light")}
+          style={styles.iconContainer}
+        >
+          {theme === "light" ? (
+            <Icon name="moon" size={30} color="#000" />
+          ) : (
+            <Icon name="sunny" size={30} color="#fff" />
+          )}
+        </TouchableOpacity>
+        <Screen />
+      </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: myColors.light,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+
+  iconContainer: {
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    padding: 10,
   },
 });
